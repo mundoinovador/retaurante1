1f5a6e1677be5670195c7ec6d1ab58c7543d5e86
= <=>
2025-10-05 01:24:06 -0300

    ajuste do border radius da imagem

--- src/components/react/ContainerImage.tsx
+++ src/components/react/ContainerImage.tsx
@@ -2,11 +2,11 @@ import { AspectRatio } from "@components/ui/aspect-ratio";
 
 function ContainerImage() {
   return (
-    <AspectRatio ratio={16 / 9} className="bg-muted rounded-lg">
+    <AspectRatio ratio={16 / 9} className="bg-muted rounded-[2rem]">
       <img
         src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
         alt="Photo by Drew Beamer"
-        className="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
+        className="h-full w-full rounded-[2rem] object-cover dark:brightness-[0.2] dark:grayscale"
       />
     </AspectRatio>
   );
