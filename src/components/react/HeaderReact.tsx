26429446a9e0a960439df612f6f702cc2da99ee7
= <=>
2025-10-07 01:12:31 -0300

    ajuste no heaer e na primeira section

--- src/components/react/HeaderReact.tsx
+++ src/components/react/HeaderReact.tsx
@@ -5,84 +5,103 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbSeparator,
 } from "@components/ui/breadcrumb";
+import React from "react";
 
 function HeaderReact() {
+  const [isLargeScreen, setIsLargeScreen] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <div className="flex gap-4">
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="/" className="link-second-header-check">
-                Inicio
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+    <div>
+      {isLargeScreen ? (
+        <Breadcrumb>
+          <BreadcrumbList>
+            <div className="flex gap-4">
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="/" className="link-second-header-check">
+                    Inicio
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-          {/* <BreadcrumbSeparator /> */}
+              {/* <BreadcrumbSeparator /> */}
 
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="#" className="link-second-header">
-                História
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="#" className="link-second-header">
+                    História
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-          {/* <BreadcrumbSeparator /> */}
+              {/* <BreadcrumbSeparator /> */}
 
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="#" className="link-second-header">
-                Endereço e História
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="#" className="link-second-header">
+                    Endereço e História
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-          {/* <BreadcrumbSeparator /> */}
+              {/* <BreadcrumbSeparator /> */}
 
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="#" className="link-second-header">
-                História
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="#" className="link-second-header">
+                    História
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-          {/*  <BreadcrumbSeparator /> */}
+              {/*  <BreadcrumbSeparator /> */}
 
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="#" className="link-second-header">
-                Cardápio
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="#" className="link-second-header">
+                    Cardápio
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-          {/* <BreadcrumbSeparator /> */}
+              {/* <BreadcrumbSeparator /> */}
 
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="#" className="link-second-header">
-                Festa em Casa
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="#" className="link-second-header">
+                    Festa em Casa
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-          {/* <BreadcrumbSeparator /> */}
+              {/* <BreadcrumbSeparator /> */}
 
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <a href="#" className="link-second-header">
-                Seja um Revendedor
-              </a>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </div>
-      </BreadcrumbList>
-    </Breadcrumb>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <a href="#" className="link-second-header">
+                    Seja um Revendedor
+                  </a>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </div>
+          </BreadcrumbList>
+        </Breadcrumb>
+      ) : (
+        <p>menu</p>
+      )}
+    </div>
   );
 }
 
