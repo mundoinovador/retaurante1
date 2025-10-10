6653c37e1d1a93f2210976af8321332850ee0cc5
= <=>
2025-10-09 23:45:35 -0300

    ajuste na responsividade animada

--- src/components/react/ContainerImage.tsx
+++ src/components/react/ContainerImage.tsx
@@ -1,13 +1,16 @@
 import { AspectRatio } from "@components/ui/aspect-ratio";
 
-function ContainerImage() {
+function ContainerImage({ urlImg }: { urlImg: string }) {
   return (
     <AspectRatio
       ratio={16 / 9}
       className="bg-muted rounded-[.5rem] md:rounded-[2rem]"
     >
       <img
-        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        src={
+          urlImg ||
+          "https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        }
         alt="Photo by Drew Beamer"
         className="h-full w-full rounded-[.5rem] md:rounded-[2rem] object-cover dark:brightness-[0.2] dark:grayscale"
       />
